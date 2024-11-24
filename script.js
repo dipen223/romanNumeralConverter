482b0fc6579e24dbf2feb0d5e8aded0c551e0940
dipen223 <dipenbhandari205@outlook.com>
2024-11-23 19:29:25 -0600

    Add a romanNumeralConverter function and output function
    the main logic of the program exists in the mentioned function,and output function shows the actual result in outputWrapper.

--- script.js
+++ script.js
@@ -2,6 +2,40 @@ let numberInput = document.getElementById("number")
 let convertBtn = document.getElementById("convert-btn");
 let outputWrapper = document.getElementById("output-container");
 
+// main logic of the program
+let romanNUmeralConverter = (num) =>{
+    let allRomanNumberSigns = [
+       ['M',1000],
+       ['CM',900],
+       ['D',500],
+       ['CD',400],
+       ['C',100],
+       ['XC',90],
+       ['L',50],
+       ['XL',40],
+       ['X',10],
+       ['V',5],
+       ['IV',4],
+       ['I',1]
+];
+    const romanNumber = [];
+    allRomanNumberSigns.forEach( function (arr){
+        while(num >= arr[1]){
+            romanNumber.push(arr[0]);
+            num -=  arr[1];
+        }
+    });
+    output(romanNumber.join(''));
+
+    };
+
+// output 
+const  output = (romanNumber) =>{
+    outputWrapper.style.display="block";
+    outputWrapper.innerHTML = `<p>${romanNumber}</p>`
+}
+
+// input valdation and ouput
 let inputValidator = (e) => {
     e.preventDefault();
     let number = Number(numberInput.value);
@@ -18,6 +52,9 @@ let inputValidator = (e) => {
         outputWrapper.style.display="block";
            outputWrapper.innerHTML = `<p>Please enter a number less than or equal to 1.</p>`
         
+    }
+    else{
+        romanNUmeralConverter(number);
     }
 }
 
