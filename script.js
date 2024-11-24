21611f135038307bff4c75db6e64ba0f41803c61
dipen223 <dipenbhandari205@outlook.com>
2024-11-23 18:06:52 -0600

    Add input validation and output container
    Check for numbers that are negative,invalid input,and numbers that are greater than or equal to 3999

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
 let numberInput = document.getElementById("number")
 let convertBtn = document.getElementById("convert-btn");
+let outputWrapper = document.getElementById("output-container");
+
+let inputValidator = (e) => {
+    e.preventDefault();
+    let number = Number(numberInput.value);
+    if (number == "" ){
+           outputWrapper.style.display="block";
+           outputWrapper.innerHTML = `<p>Please enter a valid number.</p>`
+    }
+    else if (number < 0) {
+        outputWrapper.style.display="block";
+        outputWrapper.innerHTML = `<p>Please enter a number greater than or equal to 1. </p>`
+    }
+    
+    else if (number >= 4000){
+        outputWrapper.style.display="block";
+           outputWrapper.innerHTML = `<p>Please enter a number less than or equal to 1.</p>`
+        
+    }
+}
+
+
+const outputContainer = () =>{
+    outputWrapper.innerHTML = "";
+    outputWrapper.innerHTML += `2
+    <p>
+    Please enter a valid number.
+
+    </p>
+    `
+
+}
+
+convertBtn.addEventListener("click",inputValidator)
